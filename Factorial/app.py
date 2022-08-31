03a86e87556bf7a287bbeb7e75ef4bfb687ca862
Mahdi EttehadNejad <99660553+mahdi-Eth@users.noreply.github.com>
2022-08-31 07:38:27 +0430

    Update app.py

--- Factorial/app.py
+++ Factorial/app.py
@@ -2,8 +2,7 @@ def factorial(n):
     factorial = 1
     if n == 0: return print(1)
     for i in range(2,n+1):
-        x = factorial * i
-        factorial = x
+        factorial *= i
     print(factorial)
 
 
